ce1c14219277c25ff917612a1fc624eb5d87d199
paiementstephanie <stephanie.paiement@rogers.com>
2016-03-08 17:51:33 -0500

    changed indents and spacing

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
 var $belowHeading = $('<h1>');
 var $body = $('body');
-var $belowList = $('<ul>')
+var $belowList = $('<ul>');
 var $aboveHeading = $('<h1>');
-var $aboveList = $('<ul>')
+var $aboveList = $('<ul>');
 
-
-$belowHeading.html('Below Ground Veggies');
+$belowHeading.html('Below ground veggies');
 
 $body.append($belowHeading);
 
 $body.append($belowList);
 
-$aboveHeading.html('Above Ground Veggies');
+$aboveHeading.html('Above ground veggies');
 
 $body.append($aboveHeading);
 
 $body.append($aboveList);
 
 $('ul li').each(function () {
-	if ($(this).hasClass('below')){
-		$belowList.append($(this));
-	} else {
-		$aboveList.append($(this));
-	}
+  if ($(this).hasClass('below')) {
+    $belowList.append($(this));
+  } else {
+    $aboveList.append($(this));
+  }
 });
-
-
